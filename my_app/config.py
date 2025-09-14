b70d0f59e91291b3c34ea186fdfc5e55d48b2025
andres <desarrollolibre.net@gmail.com>
2025-09-14 13:21:50 -0400

    ProdConfig

--- my_app/config.py
+++ my_app/config.py
@@ -3,12 +3,13 @@ class Config(object):
     pass
 
 class ProdConfig(Config):
-    DEBUG = False
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:twgYYhRdwyuEzAjuxuGYXeVvTdHulUUe@mysql.railway.internal:3306/railway" 
 
 class DevConfig(Config):
     DEBUG = True
     # SQLALCHEMY_DATABASE_URI="mysql+pymysql://sail:password@localhost:3306/testing" 
-    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:twgYYhRdwyuEzAjuxuGYXeVvTdHulUUe@mysql.railway.internal:3306/railway" 
+    
     SECRET_KEY='SECRET_KEY'
 
 class TestingConfig(DevConfig):
