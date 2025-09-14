c9465e39cfec661cabb29ec1099750f3193eeb57
andres <desarrollolibre.net@gmail.com>
2025-09-14 13:39:56 -0400

    borrar migraciones

--- my_app/config.py
+++ my_app/config.py
@@ -8,7 +8,7 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI="mysql+pymysql://sail:password@localhost:3306/testing" 
+    SQLALCHEMY_DATABASE_URI="mysql+pymysql://sail:password@localhost:3306/testing" 
     
     SECRET_KEY='SECRET_KEY'
 
