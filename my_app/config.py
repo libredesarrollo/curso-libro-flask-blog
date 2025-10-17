100722cf9baf2cc89c7b0ec8c30f594d6096a460
andres <desarrollolibre.net@gmail.com>
2025-10-17 08:41:40 -0400

    sqlite

--- my_app/config.py
+++ my_app/config.py
@@ -1,5 +1,7 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     # Load secret key from environment variable. Provide a default for development.
     SECRET_KEY = os.getenv('SECRET_KEY', 'a-default-dev-secret-key')
@@ -7,7 +9,7 @@ class Config(object):
 class ProdConfig(Config):
     DEBUG = False
     # Load the production database URI from an environment variable
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', "PRO"  )
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI',  "sqlite:///" + os.path.join(basedir, "dev.db") )
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:____@mysql.railway.internal:3306/railway"
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:____@shuttle.proxy.rlwy.net:17040/railway" # conectarse desde terminal a contenedor
     
